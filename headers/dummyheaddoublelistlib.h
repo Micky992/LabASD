db89737fe10d6dee03849970e8c4bd2933072c88
Babbiorsetto <aleruby97@hotmail.it>
2018-05-29 17:00:49 +0200

    Aggiunta libreria per dummy-headed double linked list

---
+++ headers/dummyheaddoublelistlib.h
@@ -0,0 +1,41 @@
+#ifndef DUMMY_HEAD_DOUBLE_LIST_LIB_H
+#define DUMMY_HEAD_DOUBLE_LIST_LIB_H
+
+typedef struct dummyNode dummyNode;
+typedef struct dummyList dummyList;
+
+int listaVuotaDummy(dummyList dum);
+/*prende in ingresso un puntatore a lista doppiamente puntata circolare con elemento fittizio
+*ritorna 0 se la lista non e' vuota, 1 se e' vuota o viene passato un puntatore NULL
+*/
+dummyList nuovaListaDummy(void);
+/*ritorna un puntatore a lista puntata circolare con elemento fittizio vuota
+*/
+int aggiungiInCodaDummy(dummyList dum, int k);
+/*prende in ingresso un puntatore a lista doppiamente puntata circolare con elemento fittizio e un intero
+*inserisce l'elemento k in coda alla lista dum
+*ritorna 1 se l'inserimento e' andato a buon fine, 0 se non e' possibile inserire l'elemento
+*/
+int aggiungiInTestaDummy(dummyList dum, int k);
+/*prende in ingresso un puntatore a lista doppiamente puntata circolare con elemento fittizio
+*inserisce l'elemento k in testa alla lista dum
+*ritorna 1 se l'inserimento e' andato a buon fine, 0 se non e' possibile inserire l'elemento
+*/
+void stampaListaDummy(dummyList dum);
+/*prende in ingresso un puntatore a lista doppiamente puntata circolare con elemento fittizio
+*stampa in stdout il contenuto dei nodi della lista
+*/
+void svuotaListaDummy(dummyList dum);
+/*prende in ingresso un puntatore a lista doppiamente puntata circolare con elemento fittizio
+*elimina tutti gli elementi della lista lasciandola vuota
+*/
+dummyList deallocaListaDummy(dummyList dum);
+/*prende in ingresso un puntatore a lista doppiamente puntata circolare con elemento fittizio
+*elimina la lista
+*restituisce un puntatore a NULL
+*/
+int ricercaInListaDummy(dummyList dum, int k);
+/*prende in ingresso un puntatore a lista doppiamente puntata circolare con elemento fittizio e un intero
+*ritorna 1 se k e' presente nella lista, 0 altrimenti
+*/
+#endif
---
+++ sources/dummyheaddoublelistlib.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <dummyheaddoublelistlib.h>
+
+typedef struct dummyNode{
+  int info;
+  struct dummyNode *prev;
+  struct dummyNode *next;
+} dummyNode;
+
+typedef dummyNode* dummyList;
+
+int listaVuotaDummy(dummyList dum){
+
+  int ret = 0;
+
+  if(dum == NULL){
+    printf("ERRORE in listaVuotaDummy: il puntatore passato non contiene una lista\n");
+    ret = 1;
+  }else if(dum->next == dum){
+    ret = 1;
+  }
+
+  return ret;
+}
+
+dummyList nuovaListaDummy(void){
+
+  dummyList tmp = (dummyList)malloc(sizeof(dummyNode));
+
+  if(tmp != NULL){
+    tmp->info = -100;
+    tmp->next = tmp;
+    tmp->prev = tmp;
+  }else{
+    printf("ERRORE in nuovaListaDummy: impossibile allocare memoria per nuova lista\n");
+  }
+  return tmp;
+}
+
+int aggiungiInCodaDummy(dummyList dum, int k){
+
+  int ret = 1;
+
+  if(dum != NULL){
+
+    dummyList tmp = (dummyList)malloc(sizeof(dummyNode));
+    if(tmp != NULL){
+
+      tmp->info = k;
+      tmp->next = dum;
+      tmp->prev = dum->prev;
+      dum->prev->next = tmp;
+      dum->prev = tmp;
+    }else{
+      printf("ERRORE in aggiungiInCodaDummy: Impossibile allocare memoria per nuovo nodo\n");
+      ret = 0;
+    }
+  }else{
+    printf("ERRORE in aggiungiInCodaDummy: il puntatore passato non contiene una lista\n");
+    ret = 0;
+  }
+
+  return ret;
+}
+
+int aggiungiInTestaDummy(dummyList dum, int k){
+
+  int ret = 1;
+
+  if(dum != NULL){
+
+    dummyList tmp = (dummyList)malloc(sizeof(dummyNode));
+    if(tmp != NULL){
+
+      tmp->info = k;
+      tmp->next = dum->next;
+      tmp->prev = dum;
+      dum->next->prev = tmp;
+      dum->next = tmp;
+    }
+    else{
+      printf("ERRORE in aggiungiInTestaDummy: impossibile allocare memoria per nuovo nodo\n");
+      ret = 0;
+    }
+  }else{
+    printf("ERRORE in aggiungiInTestaDummy: il puntatore passato non contiene una lista\n");
+    ret = 0;
+  }
+  return ret;
+}
+
+void stampaListaDummyHelper(dummyList curr, dummyList dum){
+
+  if(curr!=dum){
+    printf("%d, ", curr->info);
+    stampaListaDummyHelper(curr->next, dum);
+  }
+  return;
+}
+
+void stampaListaDummy(dummyList dum){
+  stampaListaDummyHelper(dum->next, dum);
+  return;
+}
+
+void svuotaListaDummy(dummyList dum){
+
+  dummyList curr = NULL;
+  dummyList prev = NULL;
+
+  if(!listaVuotaDummy(dum)){
+
+    curr = dum->next;
+
+    while(curr != dum){
+      prev = curr;
+      curr = curr->next;
+      free(prev);
+    }
+  }
+
+  return;
+}
+
+dummyList deallocaListaDummy(dummyList dum){
+
+  if(!listaVuotaDummy(dum)){
+    svuotaListaDummy(dum);
+    free(dum);
+  }
+
+  return NULL;
+}
+
+int ricercaInListaDummy(dummyList dum, int k){
+
+  int ret = 0;
+
+  if(!listaVuotaDummy(dum)){
+
+    dummyList curr = dum->next;
+
+    while(ret == 0 && curr!=dum){
+
+      if(curr->info == k){
+        ret = 1;
+      }
+      curr = curr->next;
+    }
+  }
+
+  return ret;
+}
